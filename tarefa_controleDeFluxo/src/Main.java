7c4f864f38351dd8f722650b2725adeed8c2d612
Felipe Aguirre <exemplo@teste.com.br>
2023-07-31 16:54:41 -0300

    Second commit

--- tarefa_controleDeFluxo/src/Main.java
+++ tarefa_controleDeFluxo/src/Main.java
@@ -6,17 +6,21 @@ public class Main {
 
     }
     private static void calculoMedia() {
-        float nota1 = 7;
-        float nota2 = 8;
-        float nota3 = 6;
-        float nota4 = 8;
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite as suas 4 notas: ");
+
+        Float nota1 = Float.valueOf(s.next());
+        Float nota2 = Float.valueOf(s.next());
+        Float nota3 = Float.valueOf(s.next());
+        Float nota4 = Float.valueOf(s.next());
         float media = (nota1 +nota2 + nota3 + nota4) / 4;
         System.out.println("Sua média é: " + media);
 
-        if(media >= 7 || media <= 10){
+        if(media >= 7 && media <=10){
             System.out.println("Você está aprovado!");
             }
-        else if (media >= 5 || media < 7) {
+        else if (media >= 5 && media < 7) {
             System.out.println("Você está de recuperação!");
             }
         else if (media < 5) {
